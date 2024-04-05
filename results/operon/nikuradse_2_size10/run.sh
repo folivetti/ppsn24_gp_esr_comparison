35bc443f73866cf4cb42328b25cd506943e5e2b2
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2024-04-05 19:06:56 +0200

    Operon results Nikuradse len10 and len12

---
+++ results/operon/nikuradse_2_size10/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# TODO repeat 50 times
+for i in $(seq 1 50)
+do
+    ~/operon/build/cli/operon_nsgp --dataset ../../../datasets/nikuradse_2.csv --target target --train 0:361 \
+			       --objective mse \
+			       --enable-symbols add,mul,div,sub,inv,powabs \
+			       --generations 250 \
+			       --population-size 100 --pool-size 100 \
+			       --female-selector tournament:2 --male-selector tournament:2 \
+			       --linear-scaling=false \
+			       --iterations 50 \
+			       --show-pareto-front \
+			       --maxlength 6 \
+			       | grep -E '[0-9]+;' > run${i}.csv
+done
