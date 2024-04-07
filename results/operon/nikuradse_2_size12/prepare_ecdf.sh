7376d70c69909f5c49ffed89cc3c1fbc2974cc43
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2024-04-07 18:14:19 +0200

    Simplified expressions for GP runs for duplicate analysis.

--- results/operon/nikuradse_2_size12/prepare_ecdf.sh
+++ results/operon/nikuradse_2_size12/prepare_ecdf.sh
@@ -5,9 +5,10 @@ threshold=${1:-0.02} # set threshold or use 0.02 as default
 echo "rank;rownum;generation;idx;expr;mse"
 echo "0;0;0;0;dummy;${threshold}"
 
-for file in run*.csv
+for i in $(seq 1 50)
 do
+    file="run${i}.csv"
     # generation,individual_index,expression,fitness,nodes
     # 0;0;((((-0.022236966) + 1.1898694) - (abs((-1.1293824)) ^ (0.27311236 * log_v_k_nu))) * (((3.7352045 * log_v_k_nu) + 5.3620472) * 1.4960924));0.0421596
-    mlr --implicit-csv-header --headerless-csv-output --csv --fs ';' cat -n then filter "float(\$4) < ${threshold}" then head -n 1
+    mlr --implicit-csv-header --headerless-csv-output --csv --fs ';' --from ${file} cat -n then filter "float(\$4) < ${threshold}" then head -n 1
 done | mlr --implicit-csv-header --headerless-csv-output --csv --fs ';' sort -n 1 then cat -n
