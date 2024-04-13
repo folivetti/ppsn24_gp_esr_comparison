fc9daa7c726afd4bddceb1b7b7b78b5e6da1eeed
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2024-04-13 11:20:48 +0200

    Update heatmaps.

--- analysis-scripts/heatmap.jl
+++ analysis-scripts/heatmap.jl
@@ -1,22 +1,9 @@
-using KernelDensity
-using Interpolations
-using Plots
+# using KernelDensity
+# using Interpolations
+using Plots, ColorSchemes
 using GZip
 using CSV, Tables
 
-# #test heatmap
-# 
-# x = sort(randn(5000))
-# y = x * 2.3 + sort(randn(5000))
-# 
-# k = kde((x, y))
-# ik = InterpKDE(k)
-# 
-# z = pdf(ik, x, y)
-# 
-# heatmap(x, y, z, c = :vik)
-# 
-# savefig("./heatmap.png");
 
 # heatmap for ESR solutions with length and depth
 # $ zcat ../results/esr/nikuradse_2_size10/fittingresults_nikuradse_ranked_mse.txt.gz | head
@@ -29,34 +16,39 @@ function esr_heatmap(filename)
         for line in eachline(file)
             # println(line)
             toks = split(line, ';')
-            nll=parse(Float64, toks[3])
-            mse=parse(Float64, toks[12])        
+            nll=parse(Float64, toks[3])         # NLL is available both for RAR and Nikuradse
             length=parse(Float64, toks[14])
             depth=parse(Float64, toks[15])
 
-            if mse < 1.0 
+            if nll < 1000
                 push!(y, length)
-                push!(x, round(sqrt(mse); digits=4))
+                push!(x, round(nll))
             end
         end
+
         println("$(length(x)) $(length(y))")
-        k = kde((x,y); bandwidth=bandwidth=(0.01,1))
-        # println("$(size(k))")
-        ik = InterpKDE(k)
-        reducedx = 0:0.001:1
-        reducedy = 0:0.1:16
-        z = pdf(ik, reducedx, reducedy)
-        println("$(size(z))")
+        
+        # k = kde((x,y); bandwidth=bandwidth=(1,1))
+        # # println("$(size(k))")
+        # ik = InterpKDE(k)
+        # reducedx = 0:0.001:1
+        # reducedy = 0:0.1:16
+        # z = pdf(ik, reducedx, reducedy)
+        # println("$(size(z))")
         # z
 
         # heatmapname = replace(filename, ".gz" => "_heatmap.csv")
         # CSV.write(heatmapname,  Tables.table(z), writeheader=false)
 
         pgfplotsx()
+        # gaston()
+        # unicodeplots()
+
         # hm = heatmap(reducedx, reducedy, z, c = :vik)
         # savefig(hm, replace(filename, ".gz" => "_heatmap.png"))
 
-        hist = histogram2d(x, y; bins = (0:0.01:1,0:1:16))
+        hist = histogram2d(x, y, c=:blues; bins=(-1010:10:1000, 1:1:12))
         savefig(hist, replace(filename, ".gz" => "_histogram.png"))
+        hist
     end
 end
